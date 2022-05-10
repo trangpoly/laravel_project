b389a8f68f07fd889fa159b16b9af99637791146
Trang Phạm <tranggpt03@gmail.com>
2022-05-10 17:06:49 +0700

    update frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\LoginCotroller;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/shop', [ShopController::class, 'index']);
+
+Route::get('/login', [LoginCotroller::class, 'index']);
+
+Route::get('/cart', [CartController::class, 'index']);
+
+Route::get('/admin', [AdminController::class, 'index']);
